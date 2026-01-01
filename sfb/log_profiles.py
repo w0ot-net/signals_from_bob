f027b8640d336a5f029939c620de53114d39216e
Chris Anastasio <fladugi@yahoo.com>
2026-01-01 17:04:57 -0500

    Revert "Avoid control message cap errors in DNS responses"
    This reverts commit 04cf15c6bb657afa2e9382d1ab551fa16e5e7da1.

--- sfb/log_profiles.py
+++ sfb/log_profiles.py
@@ -112,7 +112,6 @@ LOG_PROFILES = {
             'channel.open*',
             'channel.close*',
             'channel.send_buf_*',
-            'channel.control_send_*',
             'channel.drain',
             'channel.pack',
             'channel.write_wait',

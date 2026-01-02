9fe06ee614aeb3742db0db09f8dbb36328a8d8df
Chris Anastasio <fladugi@yahoo.com>
2026-01-02 00:49:46 -0500

    Add channel close logging profile

--- sfb/config.py
+++ sfb/config.py
@@ -173,7 +173,7 @@ class Config:
     # SQLite log queue max size (0 = unbounded)
     db_log_queue: int = 0
     # Default logging profile name (None = no profile)
-    log_profile: Optional[str] = 'icmp_retransmit_debug'
+    log_profile: Optional[str] = 'channel_close_debug'
     # Enable DNS transport logging (stdout + SQLite)
     log_component_transport_dns: bool = False
     # Enable ICMP transport logging (stdout + SQLite)
